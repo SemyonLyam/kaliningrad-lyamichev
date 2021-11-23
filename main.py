212ec78ccf53eaade1c78f9f0c3df77acc31e0ed
SemyonLyam <semyon.lyam@gmail.com>
2021-11-23 19:51:34 +0200

    Git и желтые окружности

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import sys
 
-# Press F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
 
 
-# Press the green button in the gutter to run the script.
+class Example(QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+
+    def drawf(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Рисование')
+
+        self.coords_ = [0, 0]
+        self.setMouseTracking(True)
+        self.qp = QPainter()
+        self.flag = False
+        self.status = None
+
+        self.show()
+
+    def mouseMoveEvent(self, event):
+        self.coords_ = [event.x(), event.y()]
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.status = 1
+        elif event.button() == Qt.RightButton:
+            self.status = 2
+        self.drawf()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.status = 3
+            self.drawf()
+
+    def draw(self):
+        r = randint(1, 100)
+        if self.status == 1:
+            self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            self.qp.drawEllipse(self.coords_[0], self.coords_[1], r, r)
+        elif self.status == 2:
+            self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            self.qp.drawRect(self.coords_[0], self.coords_[1], r, r)
+        elif self.status == 3:
+            self.qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            coord = [(self.coords_[0] - r, self.coords_[1] - r),
+                     (self.coords_[0] + r, self.coords_[1] - r),
+                     (self.coords_[0], self.coords_[1] + r)]
+            self.qp.drawLine(*coord[0], *coord[1])
+            self.qp.drawLine(*coord[1], *coord[2])
+            self.qp.drawLine(*coord[2], *coord[0])
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec())
